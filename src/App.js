e25e0db7892e822a451be1c6ce36fee940479702
agda.lopes <agda.domingos@student.unicv.edu.cv>
2024-03-13 22:38:02 -0100

    theme provadir data and utils

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import styled, {ThemeProvider} from 'styled-components';
+import { darkTheme } from './utils/Theme';
+
+const Body = styled.div `
+  background-color: ${({theme})=> theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={darkTheme}>
       portifolio
-    </div>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+// 9min5
